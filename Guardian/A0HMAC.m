fb60d86694c473d02aadb5382a3e9de9bd2950e7
Nicolas Ulrich <nikolaseu@users.noreply.github.com>
2016-10-04 17:54:00 -0300

    Add A0HMAC, wrapper for CommonCrypto HMAC

---
+++ Guardian/A0HMAC.m
@@ -0,0 +1,66 @@
+// A0HMAC.m
+//
+// Copyright (c) 2016 Auth0 (http://auth0.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import <A0HMAC.h>
+#import <CommonCrypto/CommonHMAC.h>
+
+@interface A0HMAC ()
+@property (readonly, nonatomic) CCHmacAlgorithm algorithm;
+@property (readonly, strong, nonatomic) NSData *key;
+@end
+
+@implementation A0HMAC
+
+- (instancetype)initWithAlgorithm:(NSString *)algorithm andKey:(NSData *)key {
+    self = [super init];
+    if (self) {
+        _key = key;
+        const NSString * alg = algorithm.lowercaseString;
+        if ([@"sha1"  isEqual: alg]) {
+            _algorithm = kCCHmacAlgSHA1;
+            _digestLength = CC_SHA1_DIGEST_LENGTH;
+        } else if ([@"sha256"  isEqual: alg]) {
+            _algorithm = kCCHmacAlgSHA256;
+            _digestLength = CC_SHA256_DIGEST_LENGTH;
+        } else if ([@"sha512"  isEqual: alg]) {
+            _algorithm = kCCHmacAlgSHA512;
+            _digestLength = CC_SHA512_DIGEST_LENGTH;
+        } else {
+            self = nil;
+        }
+    }
+    return self;
+}
+
+- (NSData *)sign:(NSData *)data {
+    uint8_t * hashBytes = malloc(_digestLength * sizeof(uint8_t));
+    if (!hashBytes) {
+        return nil;
+    }
+    memset(hashBytes, 0x0, _digestLength);
+    CCHmac(_algorithm, _key.bytes, _key.length, data.bytes, data.length, hashBytes);
+    NSData *hash = [NSData dataWithBytes:hashBytes length:_digestLength];
+    free(hashBytes);
+    return hash;
+}
+
+@end
