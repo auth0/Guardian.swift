24ba3e14de1bf3b1bc80a5cced3d35544bbe021a
Hernan Zalazar <hernan.zalazar@gmail.com>
2018-07-19 17:36:36 -0300

    Update in sample to fully use iOS 10 Notifications

--- GuardianApp/AppDelegate.swift
+++ GuardianApp/AppDelegate.swift
@@ -58,6 +58,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             options: [])
 
         // Request for AuthZ
+        UNUserNotificationCenter.current().delegate = self
         UNUserNotificationCenter.current().requestAuthorization(options: [.badge, .sound]) { granted, error in
             guard granted else {
                 return print("Permission not granted")
@@ -92,62 +93,50 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         print(error)
     }
 
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
-        // when the app is open and we receive a push notification
-        print(userInfo)
-
-        if let notification = Guardian.notification(from: userInfo) {
-            print(notification)
-            
-            let notificationController = rootController?.storyboard?.instantiateViewController(withIdentifier: "NotificationView") as! NotificationController
-            notificationController.notification = notification
-            rootController?.present(notificationController, animated: true, completion: nil)
-        }
-    }
-
-    func application(_ application: UIApplication, handleActionWithIdentifier identifier: String?, forRemoteNotification userInfo: [AnyHashable : Any], withResponseInfo responseInfo: [AnyHashable : Any], completionHandler: @escaping () -> Void) {
-        // when the app has been activated by the user selecting an action from a remote notification
-        print("identifier: \(String(describing: identifier)), userInfo: \(String(describing: userInfo))")
-
-        if let notification = Guardian.notification(from: userInfo),
-            let enrollment = AppDelegate.enrollment,
-            let identifier = identifier
-        {
-            Guardian
-                .authentication(forDomain: AppDelegate.guardianDomain, andEnrollment: enrollment)
-                .handleAction(withIdentifier: identifier, notification: notification)
-                .start { _ in
-                    completionHandler()
-            }
-        } else {
-            completionHandler()
-        }
-    }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
     var rootController: UIViewController? {
         return self.window?.rootViewController
     }
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let identifier = response.actionIdentifier
+        let userInfo = response.notification.request.content.userInfo
+
+        // when the app has been activated by the user selecting an action from a remote notification
+        print("identifier: \(identifier), userInfo: \(userInfo)")
+
+        if let notification = Guardian.notification(from: userInfo),
+            let enrollment = AppDelegate.enrollment
+        {
+            if UNNotificationDefaultActionIdentifier == identifier { // App opened from notification
+                show(notification: notification)
+                completionHandler()
+            } else { // Guardian allow/reject action
+                Guardian
+                    .authentication(forDomain: AppDelegate.guardianDomain, andEnrollment: enrollment)
+                    .handleAction(withIdentifier: identifier, notification: notification)
+                    .start { _ in completionHandler() }
+            }
+        } else { // Nothing we can handle, just not known notification
+            completionHandler()
+        }
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        print(userInfo)
+        if let notification = Guardian.notification(from: userInfo) {
+            show(notification: notification)
+        }
+        completionHandler([]) //Avoid displaying iOS UI when app in foreground
+    }
+
+    private func show(notification: Guardian.Notification) {
+        print(notification)
+
+        let notificationController = rootController?.storyboard?.instantiateViewController(withIdentifier: "NotificationView") as! NotificationController
+        notificationController.notification = notification
+        rootController?.present(notificationController, animated: true, completion: nil)
+    }
+}
